3438fa2836f32ff354b746dc584043b53db08bf1
Mohit Chauhan <mohitchauhan10@gmail.com>
2023-11-14 02:15:53 +0530

    custom not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from 'next/navigation';
+
 interface IReviewDetailsProps {
   params: {
     productId: string;
@@ -6,6 +8,9 @@ interface IReviewDetailsProps {
 }
 
 export default function ReviewDetails({ params }: IReviewDetailsProps) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
